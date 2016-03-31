3679183dade12744f308d88b583dac04ada27e04
Vanesa Untermann <vuntermann@hexacta.com>
2016-03-31 18:26:06 -0300

    Ejemplo de backend en node para obtener el id Token y access Token a partir del code enviado por google OAuth 2

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // App Settings
+  MONGO_URI: process.env.MONGO_URI || 'localhost',
+  TOKEN_SECRET: 'holamundo',
+
+  // OAuth 2.0
+  GOOGLE_SECRET: process.env.GOOGLE_SECRET || 'yIuzTC_dPVa5JkFnxf43r8-L',
+
+};
