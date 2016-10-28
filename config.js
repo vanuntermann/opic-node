9bf7af72dd7bf66dd21c7d17d6d260f4c75b7f4c
Vanesa Untermann <vuntermann@hexacta.com>
2016-10-28 15:20:32 -0300

    Se agrega gitignore y configuracion de miba_qa

--- config.js
+++ config.js
@@ -5,5 +5,10 @@ module.exports = {
 
   // OAuth 2.0
   GOOGLE_SECRET: process.env.GOOGLE_SECRET || 'yIuzTC_dPVa5JkFnxf43r8-L',
-
+  MIBA_SECRET: '2880b1a909f944e7a6f2b253f88bd346',
+  MIBA_SECRET_QA: '7bdd3a05fdb14a5c8535cc5b2580414e',
+  
+  MIBA_PROD: 'https://id.buenosaires.gob.ar/openid/',
+  MIBA_HML: 'https://id.gcba.gob.ar/openid/',
+  MIBA_QA: 'https://baid.qa.gcba.gob.ar/openid/'
 };
---
+++ server-miBA.js
@@ -0,0 +1,259 @@
+/**
+ * Satellizer Node.js Example
+ * (c) 2015 Sahat Yalkabov
+ * License: MIT
+ */
+
+var fs = require('fs');
+var path = require('path');
+var qs = require('querystring');
+
+var async = require('async');
+var bcrypt = require('bcryptjs');
+var bodyParser = require('body-parser');
+var colors = require('colors');
+var cors = require('cors');
+var express = require('express');
+var logger = require('morgan');
+var jwt = require('jwt-simple');
+var moment = require('moment');
+var mongoose = require('mongoose');
+var request = require('request');
+
+var config = require('./config');
+
+var certFile = path.resolve(__dirname, 'ssl/miba.qa.gcba.gob.ar.crt');
+
+
+var userSchema = new mongoose.Schema({
+  email: { type: String, unique: true, lowercase: true },
+  password: { type: String, select: false },
+  displayName: String,
+  picture: String,
+  google: String
+});
+
+userSchema.pre('save', function(next) {
+  var user = this;
+  if (!user.isModified('password')) {
+    return next();
+  }
+  bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function(password, done) {
+  bcrypt.compare(password, this.password, function(err, isMatch) {
+    done(err, isMatch);
+  });
+};
+
+var User = mongoose.model('User', userSchema);
+
+mongoose.connect(config.MONGO_URI);
+mongoose.connection.on('error', function(err) {
+  console.log('mongo uri: ' + config.MONGO_URI);
+  console.log('Error: Could not connect to MongoDB. Did you forget to run `mongod`?'.red);
+});
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(cors());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Force HTTPS on Heroku
+if (app.get('env') === 'production') {
+  app.use(function(req, res, next) {
+    var protocol = req.get('x-forwarded-proto');
+    protocol == 'https' ? next() : res.redirect('https://' + req.hostname + req.url);
+  });
+}
+app.use(express.static(path.join(__dirname, '../../client')));
+
+/*
+ |--------------------------------------------------------------------------
+ | Login Required Middleware
+ |--------------------------------------------------------------------------
+ */
+function ensureAuthenticated(req, res, next) {
+  if (!req.header('Authorization')) {
+    return res.status(401).send({ message: 'Please make sure your request has an Authorization header' });
+  }
+  var token = req.header('Authorization').split(' ')[1];
+
+  var payload = null;
+  try {
+    payload = jwt.decode(token, config.TOKEN_SECRET);
+  }
+  catch (err) {
+    return res.status(401).send({ message: err.message });
+  }
+
+  if (payload.exp <= moment().unix()) {
+    return res.status(401).send({ message: 'Token has expired' });
+  }
+  req.user = payload.sub;
+  next();
+}
+
+/*
+ |--------------------------------------------------------------------------
+ | Generate JSON Web Token
+ |--------------------------------------------------------------------------
+ */
+function createJWT(user) {
+  var payload = {
+    sub: user._id,
+    iat: moment().unix(),
+    exp: moment().add(14, 'days').unix()
+  };
+  return jwt.encode(payload, config.TOKEN_SECRET);
+}
+
+/*
+ |--------------------------------------------------------------------------
+ | GET /api/me
+ |--------------------------------------------------------------------------
+ */
+app.get('/api/me', ensureAuthenticated, function(req, res) {
+  User.findById(req.user, function(err, user) {
+    res.send(user);
+  });
+});
+
+/*
+ |--------------------------------------------------------------------------
+ | PUT /api/me
+ |--------------------------------------------------------------------------
+ */
+app.put('/api/me', ensureAuthenticated, function(req, res) {
+  User.findById(req.user, function(err, user) {
+    if (!user) {
+      return res.status(400).send({ message: 'User not found' });
+    }
+    user.displayName = req.body.displayName || user.displayName;
+    user.email = req.body.email || user.email;
+    user.save(function(err) {
+      res.status(200).end();
+    });
+  });
+});
+
+
+
+/*
+ |--------------------------------------------------------------------------
+ | Login with miBA
+ |--------------------------------------------------------------------------
+ */
+app.post('/auth/miBA', function(req, res, next) {
+	
+  console.log('req body:', req.body);
+	
+  var accessTokenUrl = config.MIBA_PROD + 'token/';
+  var peopleApiUrl = config.MIBA_PROD + 'userinfo/';
+  var params = {
+    code: req.body.code,
+    client_id: req.body.clientId,
+    client_secret: config.MIBA_SECRET,
+    redirect_uri: req.body.redirectUri,
+    grant_type: 'authorization_code'
+  };
+  
+  console.log('params para pedir access token', JSON.stringify(params));
+
+  var certificate = fs.readFileSync(certFile);
+  console.log('certificate' + certificate);
+  
+  // Step 1. Exchange authorization code for access token.
+  request.post(accessTokenUrl, { json: true, form: params }, function(err, response, token) {
+  //request({
+	//		url: accessTokenUrl,
+    //      method: 'POST',
+            //agentOptions: { cert: fs.readFileSync(certFile) },
+     //       json: true,
+	//		forms: params
+    //      } ,
+	//	  function(err, response, token) {
+			  
+    console.log('error', err);	
+	console.log('response', JSON.stringify(response));
+	console.log('token', JSON.stringify(token));
+
+	var idToken = token.id_token;
+    var accessToken = token.access_token;
+	console.log('id_token:' + idToken);
+	console.log('accessToken:' + accessToken);
+    var headers = { Authorization: 'Bearer ' + accessToken };
+	
+
+    // Step 2. Retrieve profile information about the current user.
+    request.get({ url: peopleApiUrl, headers: headers, json: true }, function(err, response, profile) {
+	  console.log("profile: ", profile);
+	  console.log("response: ", response.body);
+      if (profile.error) {
+        return res.status(500).send({message: profile.error.message});
+      }
+	  
+	  // Step 2-1 obtener info extra del current user:
+	  //request.get({ url: 'https://id.buenosaires.gob.ar/me/profile/', headers: headers, json: true }, function(err, response) {
+	//	  console.log("response extra info: ", response.body);
+	  //});
+	  
+      // Step 3a. Link user accounts.
+      if (req.header('Authorization')) {
+        User.findOne({ google: profile.sub }, function(err, existingUser) {
+          if (existingUser) {
+            return res.status(409).send({ message: 'There is already a Google account that belongs to you' });
+          }
+          var token = req.header('Authorization').split(' ')[1];
+          var payload = jwt.decode(token, config.TOKEN_SECRET);
+          User.findById(payload.sub, function(err, user) {
+            if (!user) {
+              return res.status(400).send({ message: 'User not found' });
+            }
+            user.google = profile.sub;
+            //user.picture = user.picture || profile.picture.replace('sz=50', 'sz=200');
+            user.displayName = user.displayName || profile.name;
+            user.save(function() {
+              var token = createJWT(user);
+              res.send({ token: token });
+            });
+          });
+        });
+      } else {
+        // Step 3b. Create a new user account or return an existing one.
+        User.findOne({ google: profile.sub }, function(err, existingUser) {
+          if (existingUser) {
+            return res.send({ token: createJWT(existingUser) });
+          }
+          var user = new User();
+          user.google = profile.sub;
+          //user.picture = profile.picture.replace('sz=50', 'sz=200');
+          user.displayName = profile.name;
+          user.save(function(err) {
+            var token = createJWT(user);
+            res.send({ token: token });
+          });
+        });
+      }
+    });
+  });
+});
+
+
+/*
+ |--------------------------------------------------------------------------
+ | Start the Server
+ |--------------------------------------------------------------------------
+ */
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
